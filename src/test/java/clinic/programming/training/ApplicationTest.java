2b2d85e0f69770996be4a8223ec4c21c114a89fc
Lei Han <leihan_99@yahoo.com>
2018-02-05 21:56:32 -0500

    check in

---
+++ src/test/java/clinic/programming/training/ApplicationTest.java
@@ -0,0 +1,22 @@
+package clinic.programming.training;
+
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class ApplicationTest {
+    private Application app;
+    
+    @Before
+    public void setup() {
+        app = new Application();
+    }
+    
+    
+    @Test
+    public void testHelo() {
+        assertTrue(4 == 4);
+    }
+
+}
